424bcd109cef51a337be78d93713e27ec8dfe2a3
kou_hin <kou_hin@cyberagent.co.jp>
2015-04-08 10:13:42 +0900

    change markdown-mode to gfm-mode

--- emacs.d/lisp/init-markdown-mode.el
+++ emacs.d/lisp/init-markdown-mode.el
@@ -7,9 +7,9 @@
 (require-package 'markdown-mode)
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.text\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
 
 (provide 'init-markdown-mode)
 
