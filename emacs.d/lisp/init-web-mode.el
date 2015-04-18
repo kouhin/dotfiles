b4695ffa595be22c1b0ccae3a76b1817ef7ad9c7
侯斌 <myvstar@gmail.com>
2015-04-18 13:57:07 +0900

    Fix broken in web-mode

--- emacs.d/lisp/init-web-mode.el
+++ emacs.d/lisp/init-web-mode.el
@@ -23,7 +23,6 @@
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js-mode-hook 'js2-minor-mode)
-(add-hook 'web-mode-hook 'js2-minor-mode)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 
 
