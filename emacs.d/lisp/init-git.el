b40404678a5a4a19c47b2426552d6c662fcbd9f7
侯斌 <myvstar@gmail.com>
2015-06-03 21:05:59 +0900

    fix magit setting

--- emacs.d/lisp/init-git.el
+++ emacs.d/lisp/init-git.el
@@ -4,7 +4,9 @@
 
 ;;; Code:
 
-(el-get-bundle magit)
+(el-get-bundle magit
+  (with-eval-after-load-feature 'magit
+	(setq magit-last-seen-setup-instructions "1.4.0")))
 
 (el-get-bundle magit-gitflow
   (with-eval-after-load-feature 'magit-gitflow
