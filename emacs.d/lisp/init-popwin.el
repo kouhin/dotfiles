a8a0247c0e09264e2a7729d0f8de75a67e3c1daf
侯斌 <myvstar@gmail.com>
2015-07-15 18:46:15 +0900

    Add popwin for magit

--- emacs.d/lisp/init-popwin.el
+++ emacs.d/lisp/init-popwin.el
@@ -12,10 +12,19 @@
   :init
   (add-hook 'after-init-hook 'popwin-mode)
   :config
-  (push '("*Ibuffer*" :position bottom :dedicated t) popwin:special-display-config)
+  (push '("*Ibuffer*" :position bottom :dedicated t)
+		popwin:special-display-config)
   (push '(direx:direx-mode :position left :width 25 :dedicated t)
 		popwin:special-display-config)
   (push '("^\*go-direx:" :regexp t :position right :width 25 :dedicated t)
+		popwin:special-display-config)
+  (push '(magit-status-mode :position bottom :noselect t :height 0.3)
+		popwin:special-display-config)
+  (push '("*magit-commit*" :noselect t :height 40 :width 80 :stick t)
+		popwin:special-display-config)
+  (push '("*magit-diff*" :noselect t :height 40 :width 80)
+		popwin:special-display-config)
+  (push '("*magit-edit-log*" :noselect t :height 15 :width 80)
 		popwin:special-display-config))
 
 (provide 'init-popwin)
