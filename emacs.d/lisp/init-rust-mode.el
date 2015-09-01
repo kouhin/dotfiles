4cbc38c96510eb30c1b600798d29e550cf39bfc9
侯斌 <myvstar@gmail.com>
2015-09-01 17:02:39 +0900

    update for rust mode

--- emacs.d/lisp/init-rust-mode.el
+++ emacs.d/lisp/init-rust-mode.el
@@ -17,13 +17,13 @@
   (add-hook 'rust-mode-hook #'racer-mode)
   (add-hook 'racer-mode-hook #'eldoc-mode)
   :config
-  (bind-key "TAB" 'company-complete racer-mode-map))
+  (bind-key "TAB" 'company-indent-or-complete-common racer-mode-map))
 
 (use-package flycheck-rust
   :ensure t
   :defer t
   :init
-  (add-hook 'rust-mode-hook 'flycheck-rust-setup))
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
 (provide 'init-rust-mode)
 
