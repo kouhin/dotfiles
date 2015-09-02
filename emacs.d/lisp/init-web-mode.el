63d4dd135eddcf4da7e403967e199ded52803e48
侯斌 <myvstar@gmail.com>
2015-09-02 14:04:02 +0900

    Add jade, stylus, coffee script support

--- emacs.d/lisp/init-web-mode.el
+++ emacs.d/lisp/init-web-mode.el
@@ -7,6 +7,9 @@
 ;; - eslint
 ;; - tern
 ;; - json-lint
+;; - coffee-script
+;; - jade
+;; - stylus
 
 ;;; Code:
 
@@ -31,6 +34,8 @@
   (flycheck-add-mode 'javascript-eslint 'js-mode)
   (flycheck-add-mode 'javascript-eslint 'js2-mode)
 
+  (add-to-list 'company-backends 'company-web-html)
+
   ;; disable jshint since we prefer eslint checking
   (setq-default flycheck-disabled-checkers
 				(append flycheck-disabled-checkers
@@ -53,6 +58,42 @@
 				(append flycheck-disabled-checkers
 						'(javascript-jshint json-jsonlint))))
 
+(use-package coffee-mode
+  :ensure t
+  :defer t)
+
+(use-package sws-mode
+  :ensure t
+  :defer t)
+
+(use-package jade-mode
+  :ensure t
+  :defer t
+  :config
+  (add-to-list 'company-backends 'company-web-jade))
+
+(use-package stylus-mode
+  :ensure t
+  :defer t)
+
+(use-package company-web
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'web-mode-hook (lambda ()
+							 (set (make-local-variable 'company-backends) '(company-web-html company-files))
+							 (company-mode t)))
+  (defadvice company-tern (before web-mode-set-up-ac-sources activate)
+	"Set `tern-mode' based on current language before running company-tern."
+	(if (equal major-mode 'web-mode)
+		(let ((web-mode-cur-language
+			   (web-mode-language-at-pos)))
+		  (if (or (string= web-mode-cur-language "javascript")
+				  (string= web-mode-cur-language "jsx")
+				  )
+			  (unless tern-mode (tern-mode))
+			(if tern-mode (tern-mode)))))))
+
 (use-package company-tern
   :ensure t
   :defer t)
@@ -64,6 +105,15 @@
   (add-hook 'js-mode-hook (lambda() (tern-mode t)))
   (add-hook 'js2-mode-hook (lambda() (tern-mode t)))
   (add-hook 'web-mode-hook (lambda() (tern-mode t))))
+
+(use-package skewer-mode
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'js2-mode-hook 'skewer-mode)
+  (add-hook 'css-mode-hook 'skewer-css-mode)
+  (add-hook 'html-mode-hook 'skewer-html-mode))
+
 
 (provide 'init-web-mode)
 
