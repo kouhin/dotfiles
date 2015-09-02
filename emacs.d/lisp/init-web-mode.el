8ba4be93edb6e08aaa512eeb53c2f3b5252f22b1
侯斌 <myvstar@gmail.com>
2015-09-02 19:03:46 +0900

    Improve web-mode

--- emacs.d/lisp/init-web-mode.el
+++ emacs.d/lisp/init-web-mode.el
@@ -31,27 +31,23 @@
   (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+  (add-hook 'web-mode-hook (lambda()
+							 (when (equal web-mode-content-type "jsx")
+							   (flycheck-select-checker 'javascript-eslint))
+							 (set (make-local-variable 'company-backends)
+								  '((company-tern company-web-html company-css)))))
   :config
-  (add-to-list 'company-backends 'company-web-html)
   (flycheck-add-mode 'html-tidy 'web-mode)
   (flycheck-add-mode 'css-csslint 'web-mode)
   (flycheck-add-mode 'javascript-eslint 'web-mode)
-  (flycheck-add-mode 'javascript-jshint 'web-mode)
-  )
+  (setq web-mode-content-types-alist
+		'(("jsx" . "\\.js[x]?\\'"))))
 
 (use-package json-mode
   :ensure t
   :defer t
   :mode "\\.json\\'")
-
-(use-package js2-mode
-  :ensure t
-  :defer t
-  :mode "\\.js\\'"
-  :config
-  ;; disable jshint since we prefer eslint checking
-  (add-to-list 'company-backends 'company-tern))
 
 (use-package jsfmt
   :ensure t
@@ -81,17 +77,18 @@
   :init
   (add-hook 'web-mode-hook (lambda ()
 							 (set (make-local-variable 'company-backends) '(company-web-html company-files))
-							 (company-mode t)))
-  (defadvice company-tern (before web-mode-set-up-ac-sources activate)
-	"Set `tern-mode' based on current language before running company-tern."
-	(if (equal major-mode 'web-mode)
-		(let ((web-mode-cur-language
-			   (web-mode-language-at-pos)))
-		  (if (or (string= web-mode-cur-language "javascript")
-				  (string= web-mode-cur-language "jsx")
-				  )
-			  (unless tern-mode (tern-mode))
-			(if tern-mode (tern-mode)))))))
+							 (company-mode t))))
+
+(defadvice company-tern (before web-mode-set-up-ac-sources activate)
+  "Set `tern-mode' based on current language before running company-tern."
+  (if (equal major-mode 'web-mode)
+	  (let ((web-mode-cur-language
+			 (web-mode-language-at-pos)))
+		(if (or (string= web-mode-cur-language "javascript")
+				(string= web-mode-cur-language "jsx")
+				)
+			(unless tern-mode (tern-mode))
+		  (if tern-mode (tern-mode))))))
 
 (use-package company-tern
   :ensure t
@@ -113,6 +110,10 @@
   (add-hook 'css-mode-hook 'skewer-css-mode)
   (add-hook 'html-mode-hook 'skewer-html-mode))
 
+(defun web-mode-cur-language()
+  "Show current language in web-mode."
+  (interactive)
+  (message (web-mode-language-at-pos)))
 
 (provide 'init-web-mode)
 
