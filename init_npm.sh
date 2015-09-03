da9f1750024c89f2d18f186711361bf6d1dd50fa
侯斌 <myvstar@gmail.com>
2015-09-03 17:13:09 +0900

    add required npms for web development

---
+++ init_npm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+npm install babel -g
+npm install bower -g
+npm install coffee-script -g
+npm install coffeelint -g
+npm install eslint -g
+npm install babel-eslint -g
+npm install gulp -g
+npm install handlebars -g
+npm install jade -g
+npm install jsfmt -g
+npm install jshint -g
+npm install jsonlint -g
+npm install node-inspector -g
+npm install stylus -g
+npm install tern -g
