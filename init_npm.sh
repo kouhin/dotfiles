aa6fe1f3fdfd5a8b1fb325dbc35234bf98eecb64
侯斌 <myvstar@gmail.com>
2015-09-11 18:38:00 +0900

    add init-npm for developing front end

--- init_npm.sh
+++ init_npm.sh
@@ -6,6 +6,7 @@ npm install coffee-script -g
 npm install coffeelint -g
 npm install eslint -g
 npm install babel-eslint -g
+npm install csslint -g
 npm install gulp -g
 npm install handlebars -g
 npm install jade -g
@@ -15,3 +16,5 @@ npm install jsonlint -g
 npm install node-inspector -g
 npm install stylus -g
 npm install tern -g
+npm install phantomjs -g
+npm install gm -g
