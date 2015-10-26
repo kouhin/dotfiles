8bc94fbeea96c99ee8a34ad02ada414aa202a192
侯 斌 <kou_hin@cyberagent.co.jp>
2015-10-26 20:51:43 +0900

    use avy instead of ace-jump

--- emacs.d/lisp/init-ace-mode.el
+++ emacs.d/lisp/init-ace-mode.el
@@ -5,17 +5,19 @@
 
 ;;; Code:
 
-;; ace-jump-mode
-(require-package 'ace-jump-mode)
+;; ace-jump-modex
+(require-package 'avy)
 (require-package 'ace-window)
 
 (add-hook 'after-init-hook '(lambda()
-							  (global-set-key (kbd "C-c SPC") 'ace-jump-mode)
-							  (global-set-key (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+							  (global-set-key (kbd "C-z") 'avy-goto-char-2)
+							  (global-set-key (kbd "C-c s ;") 'avy-goto-char)
+							  (global-set-key (kbd "C-c s e") 'avy-goto-word-0)
+							  (global-set-key (kbd "C-c s f") 'avy-goto-line)
+							  (global-set-key (kbd "C-c s w") 'avy-goto-word-1)
+							  (define-key isearch-mode-map (kbd "C-c ;") 'avy-isearch)
+							  (avy-setup-default)
 							  (global-set-key (kbd "C-x o") 'ace-window)))
-
-(add-hook 'org-mode-hook (lambda ()
-						   (local-set-key (kbd "C-c SPC") 'ace-jump-mode)))
 
 (with-eval-after-load 'ace-jump-mode
   (ace-jump-mode-enable-mark-sync))
