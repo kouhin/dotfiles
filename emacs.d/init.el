8266be0ec63db36ffbfd3de8b8e47570a35da353
侯 斌 <kou_hin@cyberagent.co.jp>
2015-12-18 18:14:45 +0900

    add generated code by package.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,6 +6,13 @@
 
 ;;; Code:
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq gc-cons-threshold (* 128 1024 1024))
 
 (let ((minver "23.3"))
