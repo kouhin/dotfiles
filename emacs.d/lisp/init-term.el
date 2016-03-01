99e9dde312a8ff62431867a8e64223c34a7b7f8f
侯 斌 <kou_hin@cyberagent.co.jp>
2016-03-01 11:15:21 +0900

    use xterm 256color instead of ansi for term

--- emacs.d/lisp/init-term.el
+++ emacs.d/lisp/init-term.el
@@ -1,14 +1,18 @@
 ;;; init-term.el --- init term
 ;;; Commentary:
 ;;; Code:
+(require-package 'xterm-color)
+(require 'xterm-color)
 
-(setq system-uses-terminfo nil)
+(add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
+(setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
+(setq font-lock-unfontify-region-function 'xterm-color-unfontify-region)
 
-;; ansi-color
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-(add-to-list 'comint-output-filter-functions 'ansi-color-process-output)
-(add-hook 'eshell-preoutput-filter-functions 'ansi-color-filter-apply)
-(add-hook 'eshell-preoutput-filter-functions 'ansi-color-apply)
+(add-hook 'eshell-mode-hook (lambda ()
+							  (setq xterm-color-preserve-properties t)
+							  (defvar eshell-output-filter-functions)
+							  (setq eshell-output-filter-functions (remove 'eshell-handle-ansi-color eshell-output-filter-functions))))
+(add-hook 'eshell-preoutput-filter-functions 'xterm-color-filter)
 
 (provide 'init-term)
 ;;; init-term.el ends here
