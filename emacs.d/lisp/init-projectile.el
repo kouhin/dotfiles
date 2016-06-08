8e00d6aec5c1ebc6272e974c055061c96cf9c026
kou_hin <kou_hin@cyberagent.co.jp>
2016-06-08 16:49:23 +0900

    change projectile-switch-project-action to projectile-dired

--- emacs.d/lisp/init-projectile.el
+++ emacs.d/lisp/init-projectile.el
@@ -6,6 +6,8 @@
 (with-eval-after-load 'projectile
   (defvar projectile-completion-system)
   (setq projectile-completion-system 'ivy)
+  (defvar projectile-switch-project-action)
+  (setq projectile-switch-project-action 'projectile-dired)
   (defun counsel-projectile-ag ()
 	"Counsel version of projectile-ag."
 	(interactive)
