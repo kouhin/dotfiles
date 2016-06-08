bee38875ea831183b4fddad1f3deb5c872ec46b8
kou_hin <kou_hin@cyberagent.co.jp>
2016-06-08 16:45:07 +0900

    add counsel-projectile-ag

--- emacs.d/lisp/init-projectile.el
+++ emacs.d/lisp/init-projectile.el
@@ -5,7 +5,14 @@
 (require-package 'projectile)
 (with-eval-after-load 'projectile
   (defvar projectile-completion-system)
-  (setq projectile-completion-system 'ivy))
+  (setq projectile-completion-system 'ivy)
+  (defun counsel-projectile-ag ()
+	"Counsel version of projectile-ag."
+	(interactive)
+	(counsel-ag "" (projectile-project-root)))
+  (defvar projectile-mode-map)
+  (define-key projectile-mode-map (kbd "C-c p s s") 'counsel-projectile-ag))
+
 (add-hook 'after-init-hook 'projectile-global-mode)
 
 (provide 'init-projectile)
