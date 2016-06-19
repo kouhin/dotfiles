4e607f9d30afb3ed3cc4e1a9234be439c14e9a0e
侯 斌 <kou_hin@cyberagent.co.jp>
2016-06-19 23:27:38 +0900

    read PATH on init

--- emacs.d/lisp/init-exec-path.el
+++ emacs.d/lisp/init-exec-path.el
@@ -5,14 +5,12 @@
 (require-package 'exec-path-from-shell)
 (with-eval-after-load 'exec-path-from-shell
   (defvar exec-path-from-shell-check-startup-files)
-  (setq exec-path-from-shell-check-startup-files nil)
-  )
+  (setq exec-path-from-shell-check-startup-files nil))
 
-(add-hook 'after-init-hook '(lambda ()
-							  (when (memq window-system '(mac ns))
-								(exec-path-from-shell-initialize))
-							  (let ((envs '("GOROOT" "GOPATH" "RUST_SRC_PATH" "TERM")))
-								(exec-path-from-shell-copy-envs envs))))
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+(let ((envs '("GOROOT" "GOPATH" "RUST_SRC_PATH" "TERM")))
+  (exec-path-from-shell-copy-envs envs))
 
 (provide 'init-exec-path)
 ;;; init-exec-path.el ends here
