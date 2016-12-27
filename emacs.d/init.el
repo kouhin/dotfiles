fcc4efe3f455344afa3bde04cca37dd900ce48a6
kou_hin <kou_hin@cyberagent.co.jp>
2016-12-27 18:25:26 +0900

    update emacs version check

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,8 +8,8 @@
 
 (setq gc-cons-threshold (* 128 1024 1024))
 
-(let ((minver "23.3"))
-  (when (version<= emacs-version "23.1")
+(let ((minver "25.0"))
+  (when (version<= emacs-version "25.0")
 	(error "Your Emacs is too old -- this config requires v%s or higher" minver)))
 (when (version<= emacs-version "24")
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
