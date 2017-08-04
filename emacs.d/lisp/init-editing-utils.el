e7f7a99fc01750d0f877ee564297996a08456f6e
kou_hin <kou_hin@cyberagent.co.jp>
2017-08-04 13:15:03 +0900

    Remove anzu-mode

--- emacs.d/lisp/init-editing-utils.el
+++ emacs.d/lisp/init-editing-utils.el
@@ -121,10 +121,6 @@
 ;; quickrun
 (require-package 'quickrun)
 
-;; anzu
-(require-package 'anzu)
-(add-hook 'after-init-hook 'global-anzu-mode)
-
 ;; copy & paste for OSX
 (require-package 'osx-clipboard)
 (add-hook 'after-init-hook 'osx-clipboard-mode)
