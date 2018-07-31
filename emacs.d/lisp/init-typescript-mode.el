8e7bc24528489d045c1c81e0703b67faddfdefda
kou-hin <kou_hin@cyberagent.co.jp>
2018-07-31 11:07:11 +0900

    Remove duplicated config

--- emacs.d/lisp/init-typescript-mode.el
+++ emacs.d/lisp/init-typescript-mode.el
@@ -8,18 +8,14 @@
     (add-hook 'typescript-mode-hook
               (lambda ()
                 (tide-setup)
-                (flycheck-mode +1)
-                (setq-default flycheck-check-syntax-automatically '(save mode-enabled))
-                (eldoc-mode +1)))
+                (setq-default flycheck-check-syntax-automatically '(save mode-enabled))))
     ;; Tide can be used along with web-mode to edit tsx files
     (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
     (add-hook 'web-mode-hook
               (lambda ()
                 (when (string-equal "tsx" (file-name-extension buffer-file-name))
                   (tide-setup)
-                  (flycheck-mode +1)
-                  (setq-default flycheck-check-syntax-automatically '(save mode-enabled))
-                  (eldoc-mode +1))))))
+                  (setq-default flycheck-check-syntax-automatically '(save mode-enabled)))))))
 
 (provide 'init-typescript-mode)
 
