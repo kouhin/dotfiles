23c6abac7a91aa65e2e08afb05cd735f58d41669
Bin Hou <houbin217jz@gmail.com>
2019-11-05 16:05:32 +0900

    Improve ts

--- emacs.d/lisp/init-typescript-mode.el
+++ emacs.d/lisp/init-typescript-mode.el
@@ -2,14 +2,26 @@
 ;;; Commentary:
 ;;; Code:
 
-(require-package 'typescript-mode)
-(require-package 'tide)
+(depends 'typescript-mode)
+(depends 'tide)
+(depends 'add-node-modules-path)
+
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . typescript-mode))
 
 (add-hook 'typescript-mode-hook
           (lambda ()
+            (add-node-modules-path)
             (tide-setup)
             (flycheck-mode +1)
-            (add-node-modules-path)
+            (if (executable-find "eslint")
+                               (progn
+                                 (defun eslint-fix ()
+                                   "Where to display jscs-fix error output.  It can either be displayed in its own buffer, in the echo area, or not at all."
+                                   (interactive)
+                                   (message "eslint --fix the file %s" (buffer-file-name))
+                                   (shell-command (concat "eslint --fix " (buffer-file-name)))
+                                   (revert-buffer t t)))
+                             (message "Please install eslint: 'npm install -g eslint-cli'."))
             (if (projectile-project-p)
                  (progn
                    (when
@@ -21,16 +33,16 @@
             (eldoc-mode +1)
             (company-mode-on)))
 ;; Tide can be used along with web-mode to edit tsx files
-(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (tide-setup)
-              (flycheck-mode +1)
-			  (defvar flycheck-check-syntax-automatically)
-              (setq flycheck-check-syntax-automatically '(save mode-enabled))
-              (eldoc-mode +1)
-              (company-mode-on))))
+;;(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+;;(add-hook 'web-mode-hook
+;;          (lambda ()
+;;            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+;;              (tide-setup)
+;;              (flycheck-mode +1)
+;;			  (defvar flycheck-check-syntax-automatically)
+;;              (setq flycheck-check-syntax-automatically '(save mode-enabled))
+;;              (eldoc-mode +1)
+;;              (company-mode-on))))
 
 (provide 'init-typescript-mode)
 
--- emacs.d/transient/history.el
+++ emacs.d/transient/history.el
@@ -2,6 +2,8 @@
   ("-w"))
  (magit-branch nil)
  (magit-branch-configure nil)
+ (magit-cherry-pick
+  ("--ff"))
  (magit-commit nil)
  (magit-diff
   ("--no-ext-diff" "--stat"))
@@ -15,9 +17,15 @@
  (magit-pull nil)
  (magit-push nil
              ("--force-with-lease")
+             ("--force")
              ("--set-upstream")
              ("--force-with-lease" "--set-upstream"))
  (magit-rebase nil)
+ (magit-remote
+  ("-f"))
+ (magit-remote\.<remote>\.*url "git@bitbucket.org:ctw_dev/g123-psp-admin.git" "git@bitbucket.org:towerofpride/g123-psp-admin.git" "git@bitbucket.org:ctw_dev/g123-psp.git" "git@bitbucket.org:towerofpride/g123-psp.git")
  (magit-reset nil)
  (magit-run nil)
- (magit-stash nil))
+ (magit-stash nil)
+ (magit-submodule nil)
+ (magit-tag nil))
