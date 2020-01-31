5282806dca2a603e5fae9e9a8c0888700dfda47e
Bin Hou <houbin217jz@gmail.com>
2020-01-31 16:23:39 +0900

    Use flycheck (lsp-ui + eslint)

--- emacs.d/lisp/init-lsp.el
+++ emacs.d/lisp/init-lsp.el
@@ -3,21 +3,38 @@
 ;;; Code:
 
 (depends 'lsp-mode)
+(depends 'lsp-ui)
 (depends 'company-lsp)
 
-                                        ; (add-hook 'lsp-mode-hook 'lsp-ui-mode)
-(add-hook 'javascript-mode-hook #'lsp)
-(add-hook 'js-mode-hook #'lsp)
-(add-hook 'typescript-mode-hook #'lsp)
-(add-hook 'rjsx-mode-hook #'lsp)
+(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+(add-hook 'javascript-mode-hook
+  '(lambda()
+     (lsp-deferred)
+     (with-eval-after-load 'lsp-ui (flycheck-add-next-checker 'lsp-ui 'javascript-eslint))))
+(add-hook 'js-mode-hook
+  '(lambda()
+     (lsp-deferred)
+     (with-eval-after-load 'lsp-ui (flycheck-add-next-checker 'lsp-ui 'javascript-eslint))))
+(add-hook 'typescript-mode-hook
+  '(lambda()
+     (lsp-deferred)
+     (with-eval-after-load 'lsp-ui (flycheck-add-next-checker 'lsp-ui 'javascript-eslint))))
+(add-hook 'rjsx-mode-hook
+  '(lambda()
+     (lsp-deferred)
+     (with-eval-after-load 'lsp-ui (flycheck-add-next-checker 'lsp-ui 'javascript-eslint))))
+(add-hook 'js2-mode-hook '(lambda()
+     (lsp-deferred)
+     (with-eval-after-load 'lsp-ui (flycheck-add-next-checker 'lsp-ui 'javascript-eslint))))
 (add-hook 'css-mode-hook #'lsp)
-(add-hook 'js2-mode-hook #'lsp)
 (add-hook 'web-mode-hook #'lsp)
 (add-hook 'go-mode-hook #'lsp)
 (add-hook 'rust-mode-hook #'lsp)
 
+
 (with-eval-after-load 'lsp-mode
-  (require 'lsp-clients))
+  (require 'lsp-clients)
+  (setq-default lsp-prefer-flymake nil))
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
--- emacs.d/lisp/init-typescript-mode.el
+++ emacs.d/lisp/init-typescript-mode.el
@@ -7,14 +7,25 @@
 
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . typescript-mode))
 
-(add-hook 'typescript-mode-hook (lambda () (add-node-modules-path) (if (executable-find "eslint") (progn (defun eslint-fix () "Where to display jscs-fix error output.  It can either be displayed in its own buffer, in the echo area, or not at all." (interactive) (message "eslint --fix the file %s" (buffer-file-name)) (shell-command (concat "eslint --fix " (buffer-file-name))) (revert-buffer t t))) (message "Please install eslint: 'npm install -g eslint-cli'.")) (if (projectile-project-p) (progn (when
-                       (file-exists-p (expand-file-name ".prettierrc.json" (projectile-project-root)))
-                     (prettier-js-mode))
-                   ))
-			(defvar flycheck-check-syntax-automatically)
-            (setq flycheck-check-syntax-automatically '(save mode-enabled))
-            (eldoc-mode +1)
-            (company-mode-on)))
+(add-hook 'typescript-mode-hook
+  (lambda ()
+    (add-node-modules-path)
+    (if (executable-find "eslint")
+      (progn
+        (defun eslint-fix ()
+          "Where to display jscs-fix error output.  It can either be displayed in its own buffer, in the echo area, or not at all."
+          (interactive)
+          (message "eslint --fix the file %s" (buffer-file-name))
+          (shell-command (concat "eslint --fix " (buffer-file-name)))
+          (revert-buffer t t)))
+      (message "Please install eslint: 'npm install -g eslint-cli'."))
+    (if (projectile-project-p)
+      (progn (when (file-exists-p (expand-file-name ".prettierrc.json" (projectile-project-root)))
+               (prettier-js-mode))))
+    (defvar flycheck-check-syntax-automatically)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (company-mode-on)))
 
 (provide 'init-typescript-mode)
 
