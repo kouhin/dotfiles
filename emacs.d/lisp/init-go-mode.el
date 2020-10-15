aa0615bc907912b7e22c3dedc836d78562f0ed9d
kouhin <houbin217jz@gmail.com>
2020-10-15 19:30:33 +0900

    Improve go-mode

--- emacs.d/lisp/init-go-mode.el
+++ emacs.d/lisp/init-go-mode.el
@@ -1,37 +1,21 @@
 ;;; init-go-mode.el --- Init Go mode
 ;;; Commentary:
 ;; Required:
-;; - go
-;; - go-rename
-;; - go-oracle
-;; - gocode
-;; - goimports
-;; - golint
-;; - errcheck
+;; - gopls
 ;;; Code:
 
 (when (depends 'go-mode)
-  (depends 'go-rename)
-  (depends 'go-guru)
-  (depends 'gotest)
-  (depends 'go-eldoc)
-
   ;; config go-mode
   (with-eval-after-load 'go-mode
-    ;; go-imports
-    (let ((goimports (executable-find "goimports")))
-      (when goimports (setq-default gofmt-command goimports))))
+    )
   (add-hook 'go-mode-hook
     '(lambda()
        ;; load GOPATH
+       (add-hook 'before-save-hook #'lsp-format-buffer t t)
+       (add-hook 'before-save-hook #'lsp-organize-imports t t)
        (unless (getenv "GOPATH")
          (exec-path-from-shell-copy-env "GOPATH"))
-
-       ;; set eldoc
-       (go-eldoc-setup)
-
-       ;; gofmt
-       (add-hook 'before-save-hook 'gofmt-before-save))))
+       )))
 
 (provide 'init-go-mode)
 ;;; init-go-mode.el ends here
