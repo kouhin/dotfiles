30caf6d4791c283748cd1781af2a1070633c78b5
Bin Hou <houbin217jz@gmail.com>
2021-04-05 23:40:28 +0900

    hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,28 @@
+hs.loadSpoon("SpoonInstall")
+
+spoon.SpoonInstall.repos.ShiftIt = {
+   url = "https://github.com/peterklijn/hammerspoon-shiftit",
+   desc = "ShiftIt spoon repository",
+   branch = "master",
+}
+
+spoon.SpoonInstall:andUse("ShiftIt", { repo = "ShiftIt" })
+
+spoon.ShiftIt:bindHotkeys({
+  left = {{ 'ctrl', 'cmd' }, 'j' },
+  right = {{ 'ctrl', 'cmd' }, 'l' },
+  up = {{ 'ctrl', 'cmd' }, 'i' },
+  down = {{ 'ctrl', 'cmd' }, ',' },
+  upleft = {{ 'ctrl', 'cmd' }, 'u' },
+  upright = {{ 'ctrl', 'cmd' }, 'o' },
+  botleft = {{ 'ctrl', 'cmd' }, 'm' },
+  botright = {{ 'ctrl', 'cmd' }, '.' },
+  maximum = {{ 'ctrl', 'cmd' }, 'k' },
+  toggleFullScreen = {{ 'ctrl', 'shift', 'cmd' }, 'k' },
+  toggleZoom = {{ 'ctrl', 'cmd' }, 'z' },
+  center = {{ 'ctrl', 'cmd' }, 'c' },
+  nextScreen = {{ 'ctrl', 'cmd' }, 'n' },
+  previousScreen = {{ 'ctrl', 'cmd' }, 'p' },
+  resizeOut = {{ 'ctrl', 'cmd' }, '=' },
+  resizeIn = {{ 'ctrl', 'cmd' }, '-' }
+})
