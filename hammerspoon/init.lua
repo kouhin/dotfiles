8e04559dfc5b249ef7761dfcdd48ccb15708931d
kouhin <houbin217jz@gmail.com>
2021-04-22 20:31:12 +0900

    Use hammerspoon as cmd-eisuu

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -26,3 +26,29 @@ spoon.ShiftIt:bindHotkeys({
   resizeOut = {{ 'ctrl', 'cmd' }, '=' },
   resizeIn = {{ 'ctrl', 'cmd' }, '-' }
 })
+
+local simpleCmd = false
+local map = hs.keycodes.map
+local function eikanaEvent(event)
+    local c = event:getKeyCode()
+    local f = event:getFlags()
+    if event:getType() == hs.eventtap.event.types.keyDown then
+        if f['cmd'] then
+            simpleCmd = true
+        end
+    elseif event:getType() == hs.eventtap.event.types.flagsChanged then
+        if not f['cmd'] then
+            if simpleCmd == false then
+                if c == map['cmd'] then
+                    hs.keycodes.setMethod('Romaji')
+                elseif c == map['rightcmd'] then
+                    hs.keycodes.setMethod('Hiragana')
+                end
+            end
+            simpleCmd = false
+        end
+    end
+end
+
+eikana = hs.eventtap.new({hs.eventtap.event.types.keyDown, hs.eventtap.event.types.flagsChanged}, eikanaEvent)
+eikana:start()
