1bd38c97c03ea7400118a9f71495e7b862b0527f
Bin Hou <kou.h@ctw.inc>
2026-05-14 15:31:47 +0900

    Update Hammerspoon config for Eisuu.spoon 2.0

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,45 +1,45 @@
 hs.loadSpoon("SpoonInstall")
 
 spoon.SpoonInstall.repos.ShiftIt = {
-   url = "https://github.com/peterklijn/hammerspoon-shiftit",
-   desc = "ShiftIt spoon repository",
-   branch = "master",
+    url = "https://github.com/peterklijn/hammerspoon-shiftit",
+    desc = "ShiftIt spoon repository",
+    branch = "master",
 }
 
 spoon.SpoonInstall:andUse("ShiftIt", { repo = "ShiftIt" })
 
 spoon.ShiftIt:bindHotkeys({
-      left = {{ 'ctrl', 'cmd' }, 'j' },
-      right = {{ 'ctrl', 'cmd' }, 'l' },
-      up = {{ 'ctrl', 'cmd' }, 'i' },
-      down = {{ 'ctrl', 'cmd' }, ',' },
-      upleft = {{ 'ctrl', 'cmd' }, 'u' },
-      upright = {{ 'ctrl', 'cmd' }, 'o' },
-      botleft = {{ 'ctrl', 'cmd' }, 'm' },
-      botright = {{ 'ctrl', 'cmd' }, '.' },
-      maximum = {{ 'ctrl', 'cmd' }, 'k' },
-      toggleFullScreen = {{ 'ctrl', 'shift', 'cmd' }, 'k' },
-      toggleZoom = {{ 'ctrl', 'cmd' }, 'z' },
-      center = {{ 'ctrl', 'cmd' }, 'c' },
-      nextScreen = {{ 'ctrl', 'cmd' }, 'n' },
-      previousScreen = {{ 'ctrl', 'cmd' }, 'p' },
-      resizeOut = {{ 'ctrl', 'cmd', 'shift' }, '[' },
-      resizeIn = {{ 'ctrl', 'cmd', 'shift' }, ']' }
+    left = { { 'ctrl', 'cmd' }, 'j' },
+    right = { { 'ctrl', 'cmd' }, 'l' },
+    up = { { 'ctrl', 'cmd' }, 'i' },
+    down = { { 'ctrl', 'cmd' }, ',' },
+    upleft = { { 'ctrl', 'cmd' }, 'u' },
+    upright = { { 'ctrl', 'cmd' }, 'o' },
+    botleft = { { 'ctrl', 'cmd' }, 'm' },
+    botright = { { 'ctrl', 'cmd' }, '.' },
+    maximum = { { 'ctrl', 'cmd' }, 'k' },
+    toggleFullScreen = { { 'ctrl', 'shift', 'cmd' }, 'k' },
+    toggleZoom = { { 'ctrl', 'cmd' }, 'z' },
+    center = { { 'ctrl', 'cmd' }, 'c' },
+    nextScreen = { { 'ctrl', 'cmd' }, 'n' },
+    previousScreen = { { 'ctrl', 'cmd' }, 'p' },
+    resizeOut = { { 'ctrl', 'cmd', 'shift' }, '[' },
+    resizeIn = { { 'ctrl', 'cmd', 'shift' }, ']' }
 })
 
 spoon.SpoonInstall.repos.Eikana = {
-   url = "https://github.com/kouhin/Eikana.spoon",
-   desc = "Eikana spoon repository",
-   branch = "main",
+    url = "https://github.com/kouhin/Eikana.spoon",
+    desc = "Eikana spoon repository",
+    branch = "main",
 }
 
 spoon.SpoonInstall:andUse("Eikana", {
-  repo = "Eikana",
-  config = {
-    userMapping = {
-      rightshift = "com.apple.inputmethod.SCIM.WBX"
+    repo = "Eikana",
+    config = {
+        userMapping = {
+            rightshift = "com.apple.inputmethod.SCIM.WBX",
+        },
+        override = true
     },
-    override = true
-  },
-  start = true
+    start = true
 })
